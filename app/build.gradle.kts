dc0b86da83ba5605f47064eef5e08f570fdedb75
asil <furkansariboga@protonmail.com>
2026-03-22 14:26:51 +0300

    fixed monochrome app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "io.github.furkansariboga.ayik"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

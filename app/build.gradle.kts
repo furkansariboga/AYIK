9568fc5d71c61bbdeca20b07af4951b0914aa414
asil <furkansaribogaci@gmail.com>
2026-05-04 00:26:32 +0300

    fixed some localizations - closed testing v6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "io.github.furkansariboga.ayik"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.3"
+        versionCode = 6
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
